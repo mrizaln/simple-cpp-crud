71120aef12b9d9e533abd5b8266184d0642db5c5
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-01-04 00:33:17 +0700

    Add function that gets integer from line and remove operator>>

--- cin_wrapper.hpp
+++ cin_wrapper.hpp
@@ -4,9 +4,14 @@
 #include <functional>
 #include <iostream>
 #include <limits>
+#include <optional>
 #include <stdexcept>
+#include <type_traits>
 #include <utility>
 
+// CinWrapper, self-recovering `std::cin` wrapper, which ignores all errors and recovers from them.
+// fail status is saved as bool that store last operation status, check it using `previousFail()` method.
+// the wrapper main purpose is to detect `std::cin.eof()` and call a callback function if it's set.
 class CinWrapper
 {
 private:
@@ -20,38 +25,73 @@ public:
     {
     }
 
-    template <typename T>
-    CinWrapper& operator>>(T& value)
-    {
-        if (!(std::cin >> value)) {
-            if (std::cin.eof()) {
-                if (m_onEof) {
-                    m_onEof();
-                }
-            } else if (std::cin.fail()) {
-                std::cin.clear();
-                m_previousFail = true;
-            } else {
-                std::cout << "Failed to read value or error occurred." << std::endl;
-                throw std::runtime_error{ "Unable to read value from std::cin" };
-            }
-        } else {
-            m_previousFail = false;
-        }
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        return *this;
-    }
-
     bool previousFail() const { return m_previousFail; }
 
     std::istream& getline(std::string& str)
     {
         std::getline(m_cin, str);
+        m_previousFail = !checkStatusAndRecover();
+
         return m_cin;
     }
 
-    std::istream& operator*() { return m_cin; }
-    std::istream* operator->() { return &m_cin; }
+    template <
+        typename T,
+        typename = std::enable_if_t<std::disjunction_v<
+            std::is_same<T, int>,
+            std::is_same<T, long>,
+            std::is_same<T, long long>,
+            std::is_same<T, unsigned long>,
+            std::is_same<T, unsigned long long>>>>
+    std::optional<T> getFromLine()
+    {
+        std::string line;
+        std::getline(m_cin, line);
+        m_previousFail = !checkStatusAndRecover();
+
+        if (m_previousFail) {
+            return std::nullopt;
+        }
+
+        try {
+            // clang-format off
+            if      constexpr (std::is_same_v<T, int>)                { return std::stoi  (line); }
+            else if constexpr (std::is_same_v<T, long>)               { return std::stol  (line); }
+            else if constexpr (std::is_same_v<T, long long>)          { return std::stoll (line); }
+            else if constexpr (std::is_same_v<T, unsigned long>)      { return std::stoul (line); }
+            else if constexpr (std::is_same_v<T, unsigned long long>) { return std::stoull(line); }
+            // clang-format on
+        } catch (...) {
+            // also set fail flag
+            m_previousFail = true;
+            return std::nullopt;
+        }
+    }
+
+private:
+    // returns true if std::cin is in good state, false otherwise
+    bool checkStatusAndRecover()
+    {
+        // very unlikely to happen
+        if (std::cin.bad()) {
+            throw std::runtime_error{ "Unable to read value from std::cin. Bad bit is set, can't recover!" };
+        }
+
+        // if eof is reached, std::cin can't be recovered using only standard methods
+        if (std::cin.eof()) {
+            if (m_onEof) {
+                m_onEof();
+            }
+            return false;
+        }
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            return false;
+        }
+
+        return true;
+    }
 };
 
 #endif /* end of include guard: CIN_WRAPPER_HPP_UVHDMPJL */
