f837ae5714d418f8c78fe98647bae07ca88bdc9c
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-01-03 21:17:09 +0700

    Add a new member that store the fail status of last operation

--- cin_wrapper.hpp
+++ cin_wrapper.hpp
@@ -12,6 +12,7 @@ class CinWrapper
 private:
     std::istream&         m_cin = std::cin;
     std::function<void()> m_onEof;
+    bool                  m_previousFail;
 
 public:
     CinWrapper(std::function<void()> onEof)
@@ -29,14 +30,19 @@ public:
                 }
             } else if (std::cin.fail()) {
                 std::cin.clear();
+                m_previousFail = true;
             } else {
                 std::cout << "Failed to read value or error occurred." << std::endl;
                 throw std::runtime_error{ "Unable to read value from std::cin" };
             }
+        } else {
+            m_previousFail = false;
         }
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         return *this;
     }
+
+    bool previousFail() const { return m_previousFail; }
 
     std::istream& getline(std::string& str)
     {
