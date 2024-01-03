0e8dc4fcd3261038a6f28207f8d516dcb413a415
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-01-04 00:39:00 +0700

    Add Windows console initialization that enable ANSI escape sequences
    I heavily used ANSI escape sequences to make the application more interactive on the console. Might use ncurses in the future if requirements can't be implemented using ANSI escape sequences.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include "crud.hpp"
 
+// enable virtual terminal processing on windows to be able to use ANSI escape sequences
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+#    define WINDOWS_USED
+#    include <windows.h>
+#endif
+
 int main()
 {
+#ifdef WINDOWS_USED
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut != INVALID_HANDLE_VALUE || hOut != NULL) {
+        DWORD dwMode = 0;
+        GetConsoleMode(hOut, &dwMode);
+        dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+        SetConsoleMode(hOut, dwMode);
+    } else {
+        std::cerr << "Error: Unable to get console handle.\n";
+    }
+#endif
+
     Crud crud;
     crud.run();
 
